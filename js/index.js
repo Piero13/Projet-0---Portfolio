c97f05b182593aa9e2d523d77489d2755378d522
Pierre Fasce <lemonpiero@gmail.com>
2022-05-07 19:10:13 +0200

    init repo

---
+++ js/index.js
@@ -0,0 +1,35 @@
+let i = 0; // current slide
+let j = 6; // total slides
+
+const images = document.querySelectorAll(".projects__viewer__img img");
+const dots = document.querySelectorAll(".projects__viewer__dots button");
+
+function indicator(num) {
+    dots.forEach(function(dot) {
+        dot.style.backgroundColor = "#d9ecf2";
+    });
+    document.querySelector(".projects__viewer__dots .dot:nth-child(" + num + ")").style.backgroundColor = "#E04F5F"
+}
+
+function dot(index) {
+    images.forEach(function(image) {
+        image.classList.remove("active");
+    });
+    document.getElementById("content" + index).classList.add("active");
+    i = index - 1;
+    indicator(index);
+}
+
+function next() {
+    document.getElementById("content" + (i + 1)).classList.remove("active");
+    i = (j + i + 1) % j;
+    document.getElementById("content" + (i + 1)).classList.add("active");
+    indicator(i + 1);
+}
+
+function prev() {
+    document.getElementById("content" + (i + 1)).classList.remove("active");
+    i = (j + i -1) % j;
+    document.getElementById("content" + (i + 1)).classList.add("active");
+    indicator(i + 1);
+}
