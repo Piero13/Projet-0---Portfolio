672133a4ba6af43a0f92d8ba8b709c0c565cc050
Pierre Fasce <lemonpiero@gmail.com>
2022-05-10 17:45:49 +0200

    construction hamberger nav menu format mobile et tablette

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// viewer
+
 let i = 0; // current slide
 let j = 6; // total slides
 
@@ -9,7 +11,7 @@ function indicator(num) {
         dot.style.backgroundColor = "#d9ecf2";
     });
     document.querySelector(".projects__viewer__dots .dot:nth-child(" + num + ")").style.backgroundColor = "#E04F5F"
-}
+};
 
 function dot(index) {
     images.forEach(function(image) {
@@ -18,18 +20,35 @@ function dot(index) {
     document.getElementById("content" + index).classList.add("active");
     i = index - 1;
     indicator(index);
-}
+};
 
 function next() {
     document.getElementById("content" + (i + 1)).classList.remove("active");
     i = (j + i + 1) % j;
     document.getElementById("content" + (i + 1)).classList.add("active");
     indicator(i + 1);
-}
+};
 
 function prev() {
     document.getElementById("content" + (i + 1)).classList.remove("active");
     i = (j + i -1) % j;
     document.getElementById("content" + (i + 1)).classList.add("active");
     indicator(i + 1);
-}
+};
+
+// ham-menu
+let hamMenuIcon = document.getElementById("ham-menu");
+let navMenuBar = document.getElementById("nav-bar");
+let navLinks = navMenuBar.querySelectorAll("li");
+
+hamMenuIcon.addEventListener("click", () => {
+    navMenuBar.classList.toggle("active");
+    hamMenuIcon.classList.toggle("fa-times");
+});
+
+navLinks.forEach((navLinks) => {
+    navLinks.addEventListener("click", () => {
+        navMenuBar.classList.remove("active");
+        hamMenuIcon.classList.toggle("fa-times");
+    });
+});
